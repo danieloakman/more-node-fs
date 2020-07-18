93144baa048e987cc4ef435bfd8ffca7f608f161
Daniel Oakman <doakman94@gmail.com>
2020-07-18 18:28:51 +1000

    Added forEachPathSync

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.forEachPathSync = void 0;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = require("path");
+/**
+ * Synchronously loop through all paths found in or at the path parameter. This function is
+ * called recursively.
+ * @param path The starting path.
+ * @param callback Called for each path found inside the path parameter.
+ * @param ignore If specified, then will not look at paths that match this regex.
+ */
+function forEachPathSync(path, callback, options) {
+    if (!fs_1.default.existsSync(path) || (options.ignore instanceof RegExp && options.ignore.test(path))) {
+        return;
+    }
+    const stats = fs_1.default.statSync(path);
+    if (stats.isFile())
+        callback(path);
+    else if (stats.isDirectory()) {
+        for (const pathInFolder of fs_1.default.readdirSync(path))
+            forEachPathSync(path_1.join(path, pathInFolder), callback, options);
+        callback(path);
+    }
+}
+exports.forEachPathSync = forEachPathSync;
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
 import fs from 'fs';
+import { join } from 'path';
 
-export function forEachPath () {
-  
+export interface ForEachPathOptions {
+  ignore?: RegExp;
+}
+
+/**
+ * Synchronously loop through all paths found in or at the path parameter. This function is
+ * called recursively.
+ * @param path The starting path.
+ * @param callback Called for each path found inside the path parameter.
+ * @param ignore If specified, then will not look at paths that match this regex.
+ */
+export function forEachPathSync (
+  path: string, callback: (path: string) => void, options?: ForEachPathOptions
+) {
+  if (!fs.existsSync(path) || (options.ignore instanceof RegExp && options.ignore.test(path))) {
+    return;
+  }
+
+  const stats = fs.statSync(path);
+  if (stats.isFile()) callback(path);
+  else if (stats.isDirectory()) {
+    for (const pathInFolder of fs.readdirSync(path))
+      forEachPathSync(join(path, pathInFolder), callback, options);
+    callback(path);
+  }
 }
