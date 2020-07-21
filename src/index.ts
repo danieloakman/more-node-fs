4151a4a2b52605e8742f69379353b39652707735
Daniel Oakman <doakman94@gmail.com>
2020-07-21 11:17:43 +1000

    Added sync and async version of readdirDeep and the async version of forEachPath

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 import fs from 'fs';
 import { join } from 'path';
+import { promisify } from 'util';
+
+// Promisified versions of asynchronous fs functions that take a callback.
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+const unlink = promisify(fs.unlink);
+
+/**
+ * Asyncronously loop through all paths found in or at the path parameter. This function is
+ * called recursively.
+ * @param path The starting path.
+ * @param callback Called for each path found inside the path parameter. Can be an async function.
+ * @param options Optional parameters:
+ *  - ignore: If specified, then will not look at paths that match this regex.
+ * @note This is about ~30% slower compared to the synchronous version of this function. So use
+ * that instead of this unless it's necessary.
+ */
+export async function forEachPath (
+  path: string,
+  callback: (path: string, stats: fs.Stats) => void,
+  options: { ignore?: RegExp } = {}
+) {
+  if (!fs.existsSync(path) || (options.ignore instanceof RegExp && options.ignore.test(path)))
+    return;
+
+  const stats = await stat(path);
+  if (stats.isDirectory())
+    for (const pathInFolder of (await readdir(path)))
+      await forEachPath(join(path, pathInFolder.toString()), callback, options);
+  await callback(path, stats);
+}
 
 /**
  * Synchronously loop through all paths found in or at the path parameter. This function is
@@ -22,4 +53,56 @@ export function forEachPathSync (
     for (const pathInFolder of fs.readdirSync(path))
       forEachPathSync(join(path, pathInFolder), callback, options);
   callback(path, stats);
+}
+
+/**
+ * Asyncronously reads and returns all file and folder paths inside of the path parameter.
+ * @param path The path to read inside of.
+ * @param options Optional parameters:
+ *  - ignore: If specified, then will not look at paths that match this regex.
+ * @returns Every path inside of the path parameter seperated into files, directories, and anything
+ * else is put in the others property.
+ * @note This is about ~30% slower compared to the synchronous version of this function. So use
+ * that instead of this unless it's necessary.
+ */
+export async function readdirDeep (
+  path: string,
+  options: { ignore?: RegExp } = {}
+): Promise<{ files: string[], dirs: string[], others: string[] }> {
+  const result = { files: [], dirs: [], others: [] };
+  await forEachPath(path, async pathInFolder => {
+    const stats = await stat(pathInFolder);
+    if (stats.isFile())
+      result.files.push(pathInFolder);
+    else if (stats.isDirectory())
+      result.dirs.push(pathInFolder);
+    else
+      result.others.push(pathInFolder);
+  }, options);
+  return result;
+}
+
+/**
+ * Syncronously reads and returns all file and folder paths inside of the path parameter.
+ * @param path The path to read inside of.
+ * @param options Optional parameters:
+ *  - ignore: If specified, then will not look at paths that match this regex.
+ * @returns Every path inside of the path parameter seperated into files, directories, and anything
+ * else is put in the others property.
+ */
+export function readdirDeepSync (
+  path: string,
+  options: { ignore?: RegExp } = {}
+): { files: string[], dirs: string[], others: string[] } {
+  const result = { files: [], dirs: [], others: [] };
+  forEachPathSync(path, pathInFolder => {
+    const stats = fs.statSync(pathInFolder);
+    if (stats.isFile())
+      result.files.push(pathInFolder);
+    else if (stats.isDirectory())
+      result.dirs.push(pathInFolder);
+    else
+      result.others.push(pathInFolder);
+  }, options);
+  return result;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,35 @@
 'use strict';
-const { forEachPathSync } = require('../dist/index');
+const moreNodeFS = require('../dist/index');
 const { join } = require('path');
+const {
+  ok: assert
+  // deepStrictEqual: equal
+} = require('assert');
 
 describe('javascript', async () => {
-  it('forEachPathSync', () => {
+  it('forEachPath', async () => {
     const paths = [];
-    const symbolicLinks = [];
-    forEachPathSync('./'/* join(__dirname, '../') */, (path, stats) => {
-      if (stats.isSymbolicLink())
-      symbolicLinks.push(path);
-      else
+    await moreNodeFS.forEachPath(join(__dirname, '../'), (path, stats) => {
       paths.push(path);
     });
-    console.log('number of paths:', paths.length);
+    assert(paths.length > 0);
+  });
+
+  it('forEachPathSync', () => {
+    const paths = [];
+    moreNodeFS.forEachPathSync(join(__dirname, '../'), (path, stats) => {
+      paths.push(path);
+    });
+    assert(paths.length > 0);
+  });
+
+  it('readdirDeep', async () => {
+    const result = await moreNodeFS.readdirDeep(join(__dirname, '../'));
+    assert(result.files.length > 0);
+  });
+
+  it('readdirDeepSync', () => {
+    const result = moreNodeFS.readdirDeepSync(join(__dirname, '../'));
+    assert(result.files.length > 0);
   });
 });
