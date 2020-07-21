a9278f807f3415e587adb0cdb8121b7c9a4d1a94
Daniel Oakman <doakman94@gmail.com>
2020-07-21 13:22:34 +1000

    Added match optional parameter

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,18 @@ const rmdir = promisify(fs.rmdir);
  * @param callback Called for each path found inside the path parameter. Can be an async function.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  */
 export async function forEachPath (
   path: string,
   callback: (path: string, stats: fs.Stats) => void,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ) {
-  if (!fs.existsSync(path) || (options.ignore instanceof RegExp && options.ignore.test(path)))
-    return;
+  if (
+    !fs.existsSync(path) ||
+    (options.ignore instanceof RegExp && options.ignore.test(path)) ||
+    (options.match instanceof RegExp && !options.match.test(path))
+  ) return;
 
   const stats = await stat(path);
   if (stats.isDirectory())
@@ -38,14 +42,18 @@ export async function forEachPath (
  * @param callback Called for each path found inside the path parameter.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  */
 export function forEachPathSync (
   path: string,
   callback: (path: string, stats: fs.Stats) => void,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ) {
-  if (!fs.existsSync(path) || (options.ignore instanceof RegExp && options.ignore.test(path)))
-    return;
+  if (
+    !fs.existsSync(path) ||
+    (options.ignore instanceof RegExp && options.ignore.test(path)) ||
+    (options.match instanceof RegExp && !options.match.test(path))
+  ) return;
 
   const stats = fs.lstatSync(path);
   if (stats.isDirectory())
@@ -62,11 +70,12 @@ export function forEachPathSync (
  * and async function.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  */
 export async function mapPath (
   path: string,
   callback: (path: string, stats: fs.Stats) => void,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ): Promise<string[]> {
   const result = [];
   await forEachPath(path, async (pathInFolder, stats) => {
@@ -82,11 +91,12 @@ export async function mapPath (
  * @param callback Called for each path found inside the path parameter.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  */
 export function mapPathSync (
   path: string,
   callback: (path: string, stats: fs.Stats) => void,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ): string[] {
   const result = [];
   forEachPathSync(path, (pathInFolder, stats) => {
@@ -100,12 +110,13 @@ export function mapPathSync (
  * @param path The path to read inside of.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  * @returns Every path inside of the path parameter seperated into files, directories, and anything
  * else is put in the others property.
  */
 export async function readdirDeep (
   path: string,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ): Promise<{ files: string[], dirs: string[], others: string[] }> {
   const result = { files: [], dirs: [], others: [] };
   await forEachPath(path, async (pathInFolder, stats) => {
@@ -124,12 +135,13 @@ export async function readdirDeep (
  * @param path The path to read inside of.
  * @param options Optional parameters:
  *  - ignore: If specified, then will not look at paths that match this regex.
+ *  - match: If specified, then will only look at paths that match this regex.
  * @returns Every path inside of the path parameter seperated into files, directories, and anything
  * else is put in the others property.
  */
 export function readdirDeepSync (
   path: string,
-  options: { ignore?: RegExp } = {}
+  options: { ignore?: RegExp, match?: RegExp } = {}
 ): { files: string[], dirs: string[], others: string[] } {
   const result = { files: [], dirs: [], others: [] };
   forEachPathSync(path, (pathInFolder, stats) => {
